61c8928976c902917920bb1e45e0d7a2875ae77d
mrzakariarajabi <m.zakarya.rajabi@gmail.com>
2024-04-13 10:21:53 +0330

    add some notebook for run some LLM

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-hasattr
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+device = "cuda" # the device to load the model onto
+
+model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2").to(device)
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
